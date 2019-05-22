f06e7804a64ddf35a37a596ba8ee769e584e94fc
taosang1992 <taosang1992@gmail.com>
2019-05-22 16:22:00 +0800

    修改OFweek人工智能网规则

--- news_spider/spiders/ai_ofweek.py
+++ news_spider/spiders/ai_ofweek.py
@@ -7,38 +7,31 @@ from news_spider.items import NewsSpiderItem
 class NewsSpider(scrapy.Spider):
     name = 'ai_ofweek'
     allowed_domains = ['ai.ofweek.com']
-    start_urls = ['https://ai.ofweek.com']
+    start_urls = ['https://ai.ofweek.com/CAT-201718-nlp.html']
     # today = time.strftime('%Y-%m-%d', time.localtime())
 
     def parse(self, response):
-        news_list = response.xpath("//ul[@class='info-list']/li")
+        news_list = response.xpath("//div[contains(@class,'main-cont-left')]/div[@class='content']/div[contains(@class,'item-box')]")
         print(news_list)
         for info_item in news_list:
             news_item = NewsSpiderItem()
-            news_item['title'] = info_item.xpath(".//div[@class='info-left-content']/div[@class='info-left-title']/a/text()").extract_first()
-            news_item['origin_website'] = '智东西'
+            info_item_content = info_item.xpath(".//div[contains(@class,'item-box-right')]")
+            if info_item_content:
+                info_item = info_item_content
+            news_item['title'] = info_item.xpath(".//div[contains(@class,'top-title')]/a/text()").extract_first()
+            news_item['origin_website'] = 'OFweek人工智能网'
             news_item['origin_host'] = self.allowed_domains[0]
-            news_item['origin_url'] = info_item.xpath(".//div[@class='info-left-content']/div[@class='info-left-title']/a/@href").extract_first()
-            news_item['section'] = ''
-            news_item['abstract'] = info_item.xpath(".//div[@class='info-left-content']/div[@class='info-left-desc']/text()").extract_first()
-            # news_item['published_at'] = info_item.xpath(".//span/text()").extract_first().strip()
-
+            news_item['origin_url'] = info_item.xpath(".//div[contains(@class,'top-title')]/a/@href").extract_first()
+            news_item['section'] = 'OFweek人工智能网 > 自然语言处理'
+            news_item['abstract'] = ''
+            text_bottom = info_item.xpath(".//div[contains(@class,'box-right-text')]")
+            if text_bottom:
+                news_item['published_at'] = info_item.xpath(".//div[@class='box-right-text-left']/span[5]/text()").extract_first().strip()[:10]
+            else:
+                news_item['published_at'] = info_item.xpath(".//div[contains(@class,'text-bottom')]//span[5]/text()").extract_first().strip()[:10]
             # if self.today != news_item['published_at']:
             #     return
 
-            print(news_item)
+            # print(news_item)
+            yield news_item
 
-            # yield news_item
-        # for info_item in news_list:
-        #     yield {
-        #         'title': info_item.xpath(".//a/@title").extract_first(),
-        #         'origin_website': '智能电网市场',
-        #         'origin_host': self.allowed_domains[0],
-        #         'origin_url': info_item.xpath(".//a/@href").extract_first(),
-        #         'published_at': info_item.xpath(".//span/text()").extract_first()
-        #     }
-
-        # next_link = response.xpath("//div[@class='list_page']/div[@class='page']/a[@title='下一页']/@href").extract_first()
-        #
-        # if next_link:
-        #     yield scrapy.Request('http://www.chinasmartgrid.com.cn/' + next_link, callback=self.parse)
