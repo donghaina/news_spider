a7ef5e01aef9bffa2cd6bd783c176392c8e9b423
taosang1992 <taosang1992@gmail.com>
2019-05-27 19:44:56 +0800

    修改执行脚本

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #! /bin/sh
 
-scrapy crawl jianshu
+scrapy crawl ai_ofweek
+scrapy crawl ailab
+scrapy crawl aistudyblog
+scrapy crawl chinasmartgrid
+scrapy crawl csia
+scrapy crawl dramx
+scrapy crawl elecfans
+scrapy crawl semi_ac
+scrapy crawl semi_car
+scrapy crawl semi_ic
+scrapy crawl semi_news
+scrapy crawl zhidx
